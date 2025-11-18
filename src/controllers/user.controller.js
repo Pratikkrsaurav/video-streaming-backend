70c37d917d823e6aa43b11807e83231e343f2def
Pratik <pratikkrsaurav199@gmail.com>
2025-11-18 21:31:19 +0530

    add changeCurrentPassword and getCurrentUser functions, and updateAccountDetails logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -223,8 +223,61 @@ try {
 }
 })
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const  {oldPassword, newPassword } = req.body
+
+  const user = await User.findById(req.user?._id)
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+   if(!isPasswordCorrect){
+      throw new apiError(400, "Old Password is incorrect")
+   }
+
+   user.password = newPassword
+   await user.save({validateBeforeSave: false})
+
+   return res
+   .status(200)
+   .json(new apiResponse(200, {}, "Password Changed Successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+  return res
+  .status(200)
+  .json(new apiResponse(200, req.user, "Current User fetched Successfully"))
+}
+)
+
+const updateAccountDetails = asyncHandler (async (req, res) => {
+   const {fullName, email, username} = req.body
+
+   if(!(fullName || email || username)) {
+      throw new apiError(400, "Atleast one field is required to update")
+   }
+
+   const user = User.findByIdAndUpdate(
+      req.user?._id,
+      {
+         $set: {
+            fullName,
+            email,
+            username,
+         }
+      },
+      { new: true }
+   ).select("-pasword")
+
+   return res
+   .status(200)
+   .json(new apiResponse(200, user, "User Details Updated Successfully"))
+
+
+})
+
 export { registerUser,
          loginUser, 
          logoutUser,
-         refreshAccessToken
+         refreshAccessToken,
+         changeCurrentPassword,
+         getCurrentUser
  };
